c0512774a95ba7d3a5900f5203a481756593bdd7
Thomas Wendt <thoemy@gmx.net>
2015-02-22 19:44:25 +0100

    tegra3-common: ril: Add infrastructure to handle unsolicited responses
    Change-Id: If1595bed92a0434e760f59ffe73f03b0a1363a16

--- ril/telephony/java/com/android/internal/telephony/HTCTegra3RIL.java
+++ ril/telephony/java/com/android/internal/telephony/HTCTegra3RIL.java
@@ -17,11 +17,103 @@
 package com.android.internal.telephony;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.provider.Settings;
+import android.telephony.Rlog;
 
 public class HTCTegra3RIL extends RIL implements CommandsInterface {
+    private static final String LOG_TAG = "HTCTegra3RIL";
+    private static boolean T3_RILJ_LOGD = true;
+
+    public static final int RIL_UNSOL_SMART_DATA_ROAMING_STATUS = 3019;
+    public static final int RIL_UNSOL_PHONE_BOOK_READY = 3022;
+    public static final int RIL_UNSOL_SIM_HOT_SWAP = 3029;
+    public static final int RIL_UNSOL_CUSTOMIZE_SIM_INFO = 3050;
+    public static final int RIL_UNSOL_SIM_HOT_SWAP_COUNT = 3052;
+
     public HTCTegra3RIL(Context context, int networkMode, int cdmaSubscription, Integer instanceId) {
         super(context, networkMode, cdmaSubscription, instanceId);
 
         mQANElements = 5;
+    }
+
+    @Override
+    protected void processUnsolicited (Parcel p) {
+        Object ret;
+        int dataPosition = p.dataPosition(); // save off position within the Parcel
+        int response = p.readInt();
+
+        try{switch(response) {
+            case RIL_UNSOL_SMART_DATA_ROAMING_STATUS: ret = responseVoid(p); break;
+            case RIL_UNSOL_PHONE_BOOK_READY: ret = responseVoid(p); break;
+            case RIL_UNSOL_SIM_HOT_SWAP: ret = responseInts(p); break;
+            case RIL_UNSOL_CUSTOMIZE_SIM_INFO: ret = responseString(p); break;
+            case RIL_UNSOL_SIM_HOT_SWAP_COUNT: ret = responseInts(p); break;
+            default:
+                // Rewind the Parcel
+                p.setDataPosition(dataPosition);
+
+                // Forward responses that we are not overriding to the super class
+                super.processUnsolicited(p);
+                return;
+            }
+        } catch (Throwable tr) {
+            Rlog.e(RILJ_LOG_TAG, "Exception processing unsol response: " + response +
+                    "Exception:" + tr.toString());
+            return;
+        }
+
+        switch (response) {
+            case RIL_UNSOL_SMART_DATA_ROAMING_STATUS:
+                if (T3_RILJ_LOGD) unsljLog(response);
+                break;
+
+            case RIL_UNSOL_PHONE_BOOK_READY:
+                if (T3_RILJ_LOGD) unsljLog(response);
+                break;
+
+            case RIL_UNSOL_SIM_HOT_SWAP:
+                if (T3_RILJ_LOGD) unsljLogRet(response, ret);
+                break;
+
+            case RIL_UNSOL_CUSTOMIZE_SIM_INFO:
+                if (T3_RILJ_LOGD) unsljLogRet(response, ret);
+                break;
+
+            case RIL_UNSOL_SIM_HOT_SWAP_COUNT:
+                if (T3_RILJ_LOGD) unsljLogRet(response, ret);
+                break;
+        }
+    }
+
+    /**
+     * Logging functions are just here to pick up our responseToString function.
+     */
+    @Override
+    protected void unsljLog(int response) {
+        riljLog("[UNSL]< " + responseToString(response));
+    }
+
+    @Override
+    protected void unsljLogRet(int response, Object ret) {
+        riljLog("[UNSL]< " + responseToString(response) + " " + retToString(response, ret));
+    }
+
+    static String
+    responseToString(int request) {
+        switch (request) {
+            case RIL_UNSOL_SMART_DATA_ROAMING_STATUS:
+                return "RIL_UNSOL_SMART_DATA_ROAMING_STATUS";
+            case RIL_UNSOL_PHONE_BOOK_READY:
+                return "UNSOL_PHONE_BOOK_READY";
+            case RIL_UNSOL_SIM_HOT_SWAP:
+                return "UNSOL_SIM_HOT_SWAP";
+            case RIL_UNSOL_CUSTOMIZE_SIM_INFO:
+                return "UNSOL_CUSTOMIZE_SIM_INFO";
+            case RIL_UNSOL_SIM_HOT_SWAP_COUNT:
+                return "UNSOL_SIM_HOT_SWAP_COUNT";
+            default:
+                return RIL.responseToString(request);
+        }
     }
 }
